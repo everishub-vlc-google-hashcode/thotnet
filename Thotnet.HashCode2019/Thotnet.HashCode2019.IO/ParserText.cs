ff172aa564c6b12024e9befa07e230f34c8129f7
Javi Mollà <jmolla31@gmail.com>
2019-02-28 19:15:57 +0100

    Update ParserText.cs

--- Thotnet.HashCode2019/Thotnet.HashCode2019.IO/ParserText.cs
+++ Thotnet.HashCode2019/Thotnet.HashCode2019.IO/ParserText.cs
@@ -18,9 +18,32 @@ namespace Thotnet.HashCode2019.Runner
         {
             var textArray = File.ReadAllLines(path);
 
-            var duh = 1;
+            var photoList = new List<Photo>();
+
+            int.TryParse(textArray[0], out var totalPhotos);
+
+            for (int i = 1; i < textArray.Length; i++)
+            {
+                var line = textArray[i].Split(' ');
+
+                var photo = new Photo()
+                {
+                    IsVertical = (line[0] == "H") ? false : true,
+                    TagCount = Convert.ToInt32(line[1]),
+                    Tags = new List<string>()
+                };
+
+                for (int x = 2; x < textArray.Length; x++)
+                {
+                    photo.Tags.Add(line[x]);
+                }
+
+                photoList.Add(photo);
+            }
+
 
             return Enumerable.Empty<Photo>();
         }
+
     }
 }
