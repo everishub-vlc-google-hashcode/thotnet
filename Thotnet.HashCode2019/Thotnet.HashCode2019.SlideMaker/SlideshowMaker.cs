41c1836fa4d88d4579010182d09738fae221082e
Pablo Ramos Villaplana <pabravil@gmail.com>
2019-02-28 19:38:50 +0100

    Added SldieBuilder class

---
+++ Thotnet.HashCode2019/Thotnet.HashCode2019.SlideMaker/SlideshowMaker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thotnet.HashCode2019.SlideMaker
+{
+    class SlideshowMaker
+    {
+        public static List<Slide> GetSlides(Photo[] photos)
+        {
+            var slides = new List<Slide>();
+            var bufferedVerticalPhoto = 0;
+            var count = 0;
+            foreach (var photo in photos)
+            {
+                if (photo.IsVertical && bufferedVerticalPhoto != -1) bufferedVerticalPhoto = count;
+                slides.Add(new Slide(photo));
+                count++;
+            }
+            return slides;
+        }
+    }
+}
