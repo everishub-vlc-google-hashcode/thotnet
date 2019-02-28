429ca8cbe8fabc481b451c80bd4bb6bd67d38eae
Jesús Sanz <jesus.sanz.losa@everis.com>
2019-02-28 20:47:02 +0100

    Scoring improved

--- Thotnet.HashCode2019/Thotnet.HashCode2019.SlideMaker/Scoring.cs
+++ Thotnet.HashCode2019/Thotnet.HashCode2019.SlideMaker/Scoring.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Thotnet.HashCode2019.SlideMaker
+﻿namespace Thotnet.HashCode2019.SlideMaker
 {
     public static class Scoring
     {
-
         public static int Min(Slide sl1, Slide sl2)
         {
             var min = 0;
+
             var commonTags = 0;
+
             var sl1Count = sl1.Tags.Count;
             var sl2Count = sl2.Tags.Count;
-
-            // TODOS FALTA COMPROBAR LOS CASOS DE IGUALES
 
             foreach (var sl1Tag in sl1.Tags)
             {
@@ -25,30 +20,34 @@ namespace Thotnet.HashCode2019.SlideMaker
 
                 if (commonTags > sl1Count || commonTags > sl2Count)
                 {
-                    if (sl1Count <= sl2Count)
-                    {
-                        return sl1Count;
-                    }
-                    else
-                    {
-                        return sl2Count;
-                    }
+                    break;
                 }
-            }            
+            }
 
-            if ((sl1Count < sl2Count) && (sl1Count < commonTags))
+            if (commonTags >= sl1Count)
             {
-                min = sl1Count;
+                if (sl1Count <= sl2Count)
+                {
+                    return sl1Count;
+                }
+                else
+                {
+                    return sl2Count;
+                }
             }
-            else if ((sl2Count < sl1Count) && (sl2Count < commonTags))
+
+            if (commonTags >= sl2Count)
             {
-                min = sl2Count;
+                if (sl2Count <= sl1Count)
+                {
+                    return sl2Count;
+                }
+                else
+                {
+                    return sl1Count;
+                }
             }
-            else if ((commonTags < sl1Count) && (commonTags < sl2Count))
-            {
-                min = commonTags;
-            }
-            return min;
+            return commonTags;
         }
     }
 }
