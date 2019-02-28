dfb7ca5bcb895c4f834de1e0b9b4d7afc4147a77
Jesús Sanz <jesus.sanz.losa@everis.com>
2019-02-28 19:48:55 +0100

    Tags added in constructor

--- Thotnet.HashCode2019/Thotnet.HashCode2019.SlideMaker/Slide.cs
+++ Thotnet.HashCode2019/Thotnet.HashCode2019.SlideMaker/Slide.cs
@@ -7,11 +7,21 @@ namespace Thotnet.HashCode2019.SlideMaker
         public Slide(Photo ph1)
         {
             this.PhotoId1 = ph1.Id;
+            this.Tags = ph1.Tags;
         }
         public Slide(Photo ph1, Photo ph2)
         {
             this.PhotoId1 = ph1.Id;
             this.PhotoId2 = ph1.Id;
+
+            this.Tags = ph1.Tags;
+            if (ph2.Tags != null)
+            {
+                foreach (var tag in ph2.Tags)
+                {
+                    this.Tags.Add(tag);
+                }
+            }
         }
 
         public int PhotoId1 { get; set; }
