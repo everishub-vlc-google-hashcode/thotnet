cfbaece64ac1cd8bc80862d2c2871082ffbe92ac
Jesús Sanz <jesus.sanz.losa@everis.com>
2019-02-28 19:20:33 +0100

    xd

---
+++ Thotnet.HashCode2019/Thotnet.HashCode2019.SlideMaker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Thotnet.HashCode2019.SlideMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Ralph :)");
+        }
+    }
+}
