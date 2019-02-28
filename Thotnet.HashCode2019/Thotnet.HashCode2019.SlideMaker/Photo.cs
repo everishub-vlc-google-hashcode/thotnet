e860f43e4c2724a2f5479e6e44280a95476f271c
Pablo Ramos Villaplana <pabravil@gmail.com>
2019-02-28 19:32:07 +0100

    Readded Id to Photo class

--- Thotnet.HashCode2019/Thotnet.HashCode2019.SlideMaker/Photo.cs
+++ Thotnet.HashCode2019/Thotnet.HashCode2019.SlideMaker/Photo.cs
@@ -4,6 +4,7 @@ namespace Thotnet.HashCode2019.SlideMaker
 {
     public class Photo
     {
+        public int Id { get; set; }
 		public bool IsVertical { get; set; }
         public int TagCount { get; set; }
         public List<string> Tags { get; set; }
