d9e13b060b1b5b81e7288433e8e05874faa3103c
Pablo Ramos Villaplana <pabravil@gmail.com>
2019-02-28 19:06:06 +0100

    Id typo in Photo.cs

--- Thotnet.HashCode2019/Thotnet.HashCode2019.SlideMaker/Photo.cs
+++ Thotnet.HashCode2019/Thotnet.HashCode2019.SlideMaker/Photo.cs
@@ -4,7 +4,7 @@ namespace Thotnet.HashCode2019.SlideMaker
 {
     public class Photo
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public bool IsVertical { get; set; }
         public String[] tags { get; set; }
     }
