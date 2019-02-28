a0cfd42d39bdc350fd175b51efc83f5c5b480790
Pablo Ramos Villaplana <pabravil@gmail.com>
2019-02-28 19:47:46 +0100

    Completed lineal greedy slide generator

--- Thotnet.HashCode2019/Thotnet.HashCode2019.SlideMaker/SlideshowMaker.cs
+++ Thotnet.HashCode2019/Thotnet.HashCode2019.SlideMaker/SlideshowMaker.cs
@@ -11,10 +11,22 @@ namespace Thotnet.HashCode2019.SlideMaker
             var slides = new List<Slide>();
             var bufferedVerticalPhoto = 0;
             var count = 0;
+
             foreach (var photo in photos)
             {
-                if (photo.IsVertical && bufferedVerticalPhoto != -1) bufferedVerticalPhoto = count;
-                slides.Add(new Slide(photo));
+                if (!photo.IsVertical)
+                {
+                    slides.Add(new Slide(photo));
+                }
+                else if (bufferedVerticalPhoto == -1)
+                {
+                    bufferedVerticalPhoto = count;
+                }
+                else
+                {
+                    slides.Add(new Slide(photos[bufferedVerticalPhoto], photo));
+                    bufferedVerticalPhoto = -1;
+                }
                 count++;
             }
             return slides;
